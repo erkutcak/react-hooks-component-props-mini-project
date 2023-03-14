c0a44120f578e82355e2a3ddbf3856a10774031d
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-03-14 15:44:25 -0400

    final lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function Article({title, date = "January 1, 1970", preview}) {
+function timeInEmojis(minutes) {
+    const articleIncrements = minutes < 30 ? 5 : 10
+    const emoji = minutes < 30 ? '☕️' : '🍱'
+
+    let emojiString = ""
+    for (let i = 0; i < minutes; i += articleIncrements) {
+        emojiString += emoji;
+    }
+
+    return emojiString
+}
+
+function Article({title, date = "January 1, 1970", preview, minutes}) {
+    const minutesToRead = timeInEmojis(minutes)
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} - {minutesToRead} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,8 +2,9 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({posts}) {
-    const articleElements = posts.map((posts) => {
-        return <Article key={posts.id} title={posts.title} preview={posts.preview}/>
+    const articleElements = posts.map((post) => {
+        //<Article key={post.id} {...post}/>  is the same thing as writing it all out like below
+        return <Article key={post.id} title={post.title} preview={post.preview} date={post.date} minutes={post.minutes}/>
     })
     return (
         <main>
